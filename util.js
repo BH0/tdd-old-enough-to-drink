29fe2fd7100e5039b456768dadaab196cf8507b1
bh0 <bryce13h0@gmail.com>
2018-10-15 07:27:30 +0100

    end of branch-protection test

--- util.js
+++ util.js
@@ -3,7 +3,7 @@ const { countries } = require("./api");
 exports.confirmDrinkingAge = (country, age) => { 
     if (countries[country]) { 
         if (age >= countries[country]) { 
-            return `you are old enough to drink in this is a test that should not pass and thus commit to master should fail ${country}`; 
+            return `you are old enough to drink in ${country}`; 
         } else { 
             return `you are not old enough to drink in ${country}`;
         }
